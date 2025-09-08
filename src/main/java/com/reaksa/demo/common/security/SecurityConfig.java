5a97f291e6f6af3ec3923148a63fa65ea4a99c7a
reaksa <chhimchanreaksa@gmail.com>
2025-09-08 18:36:23 +0700

    refacotor: modify security config - password encoder - authentication manager builder - allow public access for only /auth/**

--- src/main/java/com/reaksa/demo/common/security/SecurityConfig.java
+++ src/main/java/com/reaksa/demo/common/security/SecurityConfig.java
@@ -1,14 +1,39 @@
 package com.reaksa.demo.common.security;
 
+import com.reaksa.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authBuilder
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+        return authBuilder.build();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,6 +45,7 @@ public class SecurityConfig {
                         authz
 //                                .requestMatchers("/api/v1/users/**").hasRole("ADMIN")
                                 .requestMatchers("/api/**").permitAll()
+                                .requestMatchers("/api/v1/auth/**").permitAll()
                                 .anyRequest()
                                 .authenticated()
                 );
