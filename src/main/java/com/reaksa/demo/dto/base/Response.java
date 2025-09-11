b95164b7704e362422015f1aa9616eb6ffb9a3a4
reaksa <chhimchanreaksa@gmail.com>
2025-09-11 19:58:49 +0700

    refactor(response): add timestamp field

--- src/main/java/com/reaksa/demo/dto/base/Response.java
+++ src/main/java/com/reaksa/demo/dto/base/Response.java
@@ -2,26 +2,31 @@ package com.reaksa.demo.dto.base;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
 
 @JsonPropertyOrder(value = {"code", "message", "description", "data"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
 public class Response {
     private String code;
     private String message;
     private String description;
     private Object data;
+    private Long timestamp;
 
     private Response(String code, String message, String description, Object data) {
         this.code = code;
         this.message = message;
         this.description = description;
         this.data = data;
+        this.timestamp = System.currentTimeMillis();
     }
 
     private Response(String code, String message, String description) {
         this.code = code;
         this.message = message;
         this.description = description;
+        this.timestamp = System.currentTimeMillis();
     }
 
     public static Response success(String message, String description) {
