daad72e8ccba6d3366addec5955da26dd365644d
reaksa <chhimchanreaksa@gmail.com>
2025-09-15 20:06:37 +0700

    chore: remove print line

--- src/main/java/com/reaksa/demo/service/security/JwtUtil.java
+++ src/main/java/com/reaksa/demo/service/security/JwtUtil.java
@@ -31,7 +31,6 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
-        System.out.println("Key : " + this.getSigningKey());
 
         return Jwts.builder()
                 .setClaims(claims)
