433db9ab3bbd4cb70f2c240f248391a72db6cc63
reaksa <chhimchanreaksa@gmail.com>
2025-09-27 16:46:35 +0700

    refactor(stock): separate concern - change response type - service layer only constains business layer

--- src/main/java/com/reaksa/demo/controller/StockController.java
+++ src/main/java/com/reaksa/demo/controller/StockController.java
@@ -1,5 +1,7 @@
 package com.reaksa.demo.controller;
 
+import com.reaksa.demo.dto.Stock.StockResponseDto;
+import com.reaksa.demo.dto.base.Response;
 import com.reaksa.demo.model.BaseResponseModel;
 import com.reaksa.demo.model.BaseResponseWithDataModel;
 import com.reaksa.demo.dto.Stock.StockDto;
@@ -7,8 +9,11 @@ import com.reaksa.demo.dto.Stock.UpdateStockDto;
 import com.reaksa.demo.service.StockService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/stocks")
@@ -17,33 +22,43 @@ public class StockController {
     private StockService stockService;
 
     @GetMapping
-    public ResponseEntity<BaseResponseWithDataModel> listStock() {
-        return stockService.listStock();
+    public ResponseEntity<Response> listStock() {
+        List<StockResponseDto> stocks = stockService.listStock();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.success("200", "success", "successfully retrieved stocks", stocks));
     }
 
     @GetMapping("/{stockID}")
-    public ResponseEntity<BaseResponseWithDataModel> getStock(@PathVariable Long stockID) {
-        return stockService.getStock(stockID);
+    public ResponseEntity<Response> getStock(@PathVariable Long stockID) {
+        StockResponseDto stock = stockService.getStock(stockID);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.success("200", "success", "stock found", stock));
     }
 
     @PostMapping
-    public ResponseEntity<BaseResponseModel> addStock(@Valid @RequestBody StockDto payload) {
-        return stockService.createStock(payload);
+    public ResponseEntity<Response> addStock(@Valid @RequestBody StockDto payload) {
+        stockService.createStock(payload);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Response.success("201", "success", "stock created"));
     }
 
-//    @PutMapping("/{stockId}")
-//    public ResponseEntity<BaseResponseModel> updateStock(@PathVariable("stockId") Long stockId, @RequestBody StockDto payload) {
-//        return stockService.updateStock(stockId, payload);
-//    }
-
     @PatchMapping("/{stockId}")
-    public ResponseEntity<BaseResponseModel> adjustQuantity(@PathVariable("stockId") Long stockId, @Valid @RequestBody
+    public ResponseEntity<Response> adjustQuantity(@PathVariable("stockId") Long stockId, @Valid @RequestBody
     UpdateStockDto payload) {
-        return stockService.adjustQuantity(stockId, payload);
+        stockService.adjustQuantity(stockId, payload);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.success("200", "success", "stock adjusted"));
     }
 
     @DeleteMapping("/{stockId}")
-    public ResponseEntity<BaseResponseModel> deleteStock(@PathVariable("stockId") Long stockId) {
-        return stockService.deleteStock(stockId);
+    public ResponseEntity<Response> deleteStock(@PathVariable("stockId") Long stockId) {
+        stockService.deleteStock(stockId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.success("200", "success", "stock deleted"));
     }
 }
--- src/main/java/com/reaksa/demo/service/StockService.java
+++ src/main/java/com/reaksa/demo/service/StockService.java
@@ -4,6 +4,7 @@ import com.reaksa.demo.dto.Stock.StockResponseDto;
 import com.reaksa.demo.entity.Product;
 import com.reaksa.demo.entity.Stock;
 import com.reaksa.demo.exception.model.ResourceNotFoundException;
+import com.reaksa.demo.exception.model.UnprocessableEntityException;
 import com.reaksa.demo.mapper.StockMapper;
 import com.reaksa.demo.model.BaseResponseModel;
 import com.reaksa.demo.model.BaseResponseWithDataModel;
@@ -31,57 +32,30 @@ public class StockService {
     @Autowired
     private StockMapper mapper;
 
-    public ResponseEntity<BaseResponseWithDataModel> listStock() {
+    public List<StockResponseDto> listStock() {
         List<Stock> stocks = stockRepository.findAll();
 
-        List<StockResponseDto> dtos = mapper.toListDto(stocks);
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseWithDataModel("success", "successfully list stock!", dtos));
+        return mapper.toListDto(stocks);
     }
 
-    public ResponseEntity<BaseResponseWithDataModel> getStock(Long stockId) {
+    public StockResponseDto getStock(Long stockId) {
 
         Stock stock = stockRepository.findById(stockId)
                 .orElseThrow(() -> new ResourceNotFoundException("stock not found with id : " + stockId));
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseWithDataModel("success", "successfully retrieve stock!", mapper.toDto(stock)));
+        return mapper.toDto(stock);
     }
 
-    public ResponseEntity<BaseResponseModel> createStock(StockDto stock) {
+    public void createStock(StockDto stock) {
         Product existingProduct = productRepository.findById(stock.getProductId())
                 .orElseThrow(() ->  new ResourceNotFoundException("product not found with id : " + stock.getProductId()));
 
         Stock  stockEntity = mapper.toEntity(stock,  existingProduct);
 
         stockRepository.save(stockEntity);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new BaseResponseModel("success", "successfully created stock"));
     }
 
-    // update stock
-    /*
-    public ResponseEntity<BaseResponseModel> updateStock(Long stockId, StockDto stock) {
-        Optional<Stock> stockEntity = stockRepository.findById(stockId);
-
-        if(stockEntity.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new BaseResponseModel("fail", "stock not found id : " + stockId));
-        }
-
-        Stock updatedStock = stockEntity.get();
-        mapper.updateEntityFromDto(updatedStock, stock);
-
-        stockRepository.save(updatedStock);
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseModel("success", "successfully updated stock id : " + stockId));
-    }
-    */
-
-    public ResponseEntity<BaseResponseModel> adjustQuantity(Long stockId, UpdateStockDto updateStock) {
+    public void adjustQuantity(Long stockId, UpdateStockDto updateStock) {
 
         Stock exsitingStock = stockRepository.findById(stockId)
                 .orElseThrow(() -> new ResourceNotFoundException("stock not found with id : " + stockId));
@@ -92,24 +66,20 @@ public class StockService {
             exsitingStock.setQuantity(newQty);
         } else if(updateStock.getOperationType() == 2) {
             if (exsitingStock.getQuantity() < updateStock.getQuantity()) {
-                return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                        .body(new BaseResponseModel("fail", "quantity to remove cannot be exceeded than existing stock"));
+                throw new UnprocessableEntityException("quantity to remove cannot be exceeded than existing stock" + exsitingStock.getQuantity());
             }
 
             int newQty = exsitingStock.getQuantity()  - updateStock.getQuantity();
 
             exsitingStock.setQuantity(newQty);
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new BaseResponseModel("fail", "operation type not supported"));
+            throw new IllegalArgumentException("operation type not supported");
         }
 
         stockRepository.save(exsitingStock);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseModel("success", "successfully updated stock"));
     }
 
-    public ResponseEntity<BaseResponseModel> deleteStock(Long stockId) {
+    public void deleteStock(Long stockId) {
         Optional<Stock> stockEntity = stockRepository.findById(stockId);
 
         if(stockEntity.isEmpty()) {
@@ -117,9 +87,6 @@ public class StockService {
         }
 
         stockRepository.deleteById(stockId);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                .body(new BaseResponseModel("success", "successfully deleted stock id : " + stockId));
     }
 
 }
