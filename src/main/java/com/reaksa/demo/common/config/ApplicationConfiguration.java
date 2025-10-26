d4974881aafb4271f977342a9de34cb863da178f
reaksa <chhimchanreaksa@gmail.com>
2025-10-26 13:53:58 +0700

    refactor: move json placeholder configuration to config file

--- src/main/java/com/reaksa/demo/common/config/ApplicationConfiguration.java
+++ src/main/java/com/reaksa/demo/common/config/ApplicationConfiguration.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 public class ApplicationConfiguration {
     private Security security;
     private Pagination pagination;
+    private JsonPlaceholder jsonPlaceholder;
 
     @Getter
     @Setter
@@ -31,6 +32,21 @@ public class ApplicationConfiguration {
 
         public String getUrlByResourse(String resourse) {
             return baseUrl.concat(uri.getOrDefault(resourse,""));
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class JsonPlaceholder {
+        private String baseUrl;
+        private HashMap<String,String> uri;
+
+        public String getPostUri() {
+            return uri.get("post");
+        }
+
+        public String getCommentsUri() {
+            return uri.get("comments");
         }
     }
 }
--- src/main/java/com/reaksa/demo/service/JsonPlaceholderService.java
+++ src/main/java/com/reaksa/demo/service/JsonPlaceholderService.java
@@ -1,8 +1,10 @@
 package com.reaksa.demo.service;
 
+import com.reaksa.demo.common.config.ApplicationConfiguration;
 import com.reaksa.demo.common.wrapper.WebClientWrapper;
 import com.reaksa.demo.dto.external.JsonPlaceholderCommentDto;
 import com.reaksa.demo.dto.external.JsonPlaceholderPostDto;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,18 +17,32 @@ public class JsonPlaceholderService {
     @Autowired
     private WebClientWrapper  webClientWrapper;
 
-    public List<JsonPlaceholderPostDto> getPosts() {
-        String uri = "https://jsonplaceholder.typicode.com/posts";
+    @Autowired
+    private ApplicationConfiguration  appConfig;
 
-        List<JsonPlaceholderPostDto> response = (List<JsonPlaceholderPostDto>)webClientWrapper.getSync(uri, List.class);
+    private String BASE_URL;
+    private String POSTS_URL;
+    private String COMMENTS_URL;
+
+    @PostConstruct
+    private void init(){
+        this.BASE_URL = appConfig.getJsonPlaceholder().getBaseUrl();
+        this.POSTS_URL = appConfig.getJsonPlaceholder().getPostUri();
+        this.COMMENTS_URL = appConfig.getJsonPlaceholder().getCommentsUri();
+    }
+
+    public List<JsonPlaceholderPostDto> getPosts() {
+        String url = BASE_URL.concat(POSTS_URL);
+
+        List<JsonPlaceholderPostDto> response = (List<JsonPlaceholderPostDto>)webClientWrapper.getSync(url, List.class);
 
         return response;
     }
 
     public List<JsonPlaceholderCommentDto> getComments() {
-        String uri = "https://jsonplaceholder.typicode.com/comments";
+        String url = BASE_URL.concat(COMMENTS_URL);
 
-        List<JsonPlaceholderCommentDto> response = (List<JsonPlaceholderCommentDto>)webClientWrapper.getSync(uri, List.class);
+        List<JsonPlaceholderCommentDto> response = (List<JsonPlaceholderCommentDto>)webClientWrapper.getSync(url, List.class);
 
         return response;
     }
