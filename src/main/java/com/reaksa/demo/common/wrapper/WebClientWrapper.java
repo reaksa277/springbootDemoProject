058ab8017e50b2e14c2f8278212f92593192aa2f
reaksa <chhimchanreaksa@gmail.com>
2025-10-26 20:16:28 +0700

    refactor(webclient-wrapper): handle error response from client api

--- src/main/java/com/reaksa/demo/common/wrapper/WebClientWrapper.java
+++ src/main/java/com/reaksa/demo/common/wrapper/WebClientWrapper.java
@@ -2,8 +2,11 @@ package com.reaksa.demo.common.wrapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -21,9 +24,14 @@ public class WebClientWrapper {
                 .uri(url)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, this::handleErrorResponse)
                 .bodyToMono(responseType)
                 .timeout(Duration.ofMillis(5000))
-                .retryWhen(Retry.backoff(3, Duration.ofMillis(5000)));
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(5000))
+                        .filter(throwable -> throwable instanceof WebClientResponseException
+                                && ((WebClientResponseException) throwable).getStatusCode().is5xxServerError()
+                        )
+                );
     }
 
     public <T> T getSync(String url, Class<T> responseType) {
@@ -32,9 +40,14 @@ public class WebClientWrapper {
                 .uri(url)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, this::handleErrorResponse)
                 .bodyToMono(responseType)
                 .timeout(Duration.ofMillis(5000))
-                .retryWhen(Retry.backoff(3, Duration.ofSeconds(1)))
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(1))
+                        .filter(throwable -> throwable instanceof WebClientResponseException
+                                && ((WebClientResponseException) throwable).getStatusCode().is5xxServerError()
+                        )
+                )
                 .block();
     }
 
@@ -49,6 +62,24 @@ public class WebClientWrapper {
                 .timeout(Duration.ofMillis(5000))
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(1)))
                 .block();
+    }
+
+    private Mono<Throwable> handleErrorResponse(ClientResponse response) {
+        return response.bodyToMono(String.class)
+                .flatMap(body -> {
+                    String errorMsg = "Client error: " + response.statusCode();
+
+                    return Mono.error(
+                            new WebClientResponseException(
+                                    errorMsg,
+                                    response.statusCode().value(), // 401, 403, 404, 400
+                                    response.statusCode().toString(), // 401 Unauthorized, 404 Not Found
+                                    null,
+                                    body.getBytes(),
+                                    null
+                            )
+                    );
+                });
     }
 
 }
